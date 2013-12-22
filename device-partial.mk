83659458e8a01b5db5f1a109a6e1743a9e16fd72
android <trevd1234@gmail.com>
2013-12-22 22:13:50 +0000

    Initial commit for vendor/ti/a80sboard

---
+++ device-partial.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	wl127x-fw-4-plt \
+	wl127x-fw-4-sr \
+	wl1271-nvs \
+	wl127x-fw-4-mr \
+	TQS_S_2.6 \
+	TQS_D_1.7 \
+	TIInit_7.2.31 \
+	hci_vhci \
+	lib80211 \
+	cfg80211 \
+	wl12xx_sdio_test \
+	btsdio \
+	scsi_wait_scan \
+	hci_uart \
+	mac80211 \
+	wl12xx \
+	bluetooth \
+	tty_hci \
+	compat \
+	btwilink \
+	rfcomm \
+	bnep \
+	hidp \
+	wl12xx_sdio \
+	gps_drv
+
